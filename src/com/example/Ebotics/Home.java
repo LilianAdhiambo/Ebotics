0551af6904f88686c030d320216288c00edc9ab7
LilianAdhiambo <lizmadhiambo@gmail.comgit config --listgit config --global user.email lizmadhiambo@gmail.com>
2014-10-09 16:59:01 +0300

    Initial commit

---
+++ src/com/example/Ebotics/Home.java
@@ -0,0 +1,13 @@
+package com.example.Ebotics;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by lilian on 10/8/14.
+ */
+public class Home extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
